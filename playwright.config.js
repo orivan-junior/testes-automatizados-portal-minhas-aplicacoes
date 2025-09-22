1a9c1d88696867a7266ec658cfec244d771347bf
Orivan Lemes dos Santos Junior <olemes@sistran.com.br>
2025-09-22 11:17:26 -0300

    Atualiza configuração do Playwright para modo headless condicional no CI e remove dependência do Chrome instalado, utilizando o Chromium do Playwright. Adiciona novas opções de lançamento para melhorar a compatibilidade em ambientes de CI.

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 }, // Define viewport para  hd
     video: 'on', // Captura vídeo para todos os testes
     ignoreHTTPSErrors: true, // Ignora erros de HTTPS
-    headless: false, // Executa no modo headless
+    headless: process.env.CI === 'true' ? true : false, // Headless no CI, visível localmente
     // Autenticação HTTP (Basic/Digest) automática quando o servidor solicitar
     httpCredentials,
     // baseURL: 'https://wwwn.dsv.bradescoseguros.com.br/pnegocios2/wps/portal/portaldenegociosnovo/!ut/p/z1/hU7LCsIwEPwWDz2aXWuR6i0IFqUVRNC6F0lr-oA2KUlU_HsDXtXObZ4MEORASjzaWrhWK9F5fqHFNU3SLS7jcI_rTYgHfjpGSRbOoiyC81iAvI0_wNH3aSyyA6o7XXzecFXM4xrIyEoaadjdeLlxbrCrAAO0zj8vLXNS9JaVN8V0VbWlZEq6AL8NNNo6yP_1YOhznFLxevLJ5A0FAdjh/dz/d5/L2dBISEvZ0FBIS9nQSEh/',
@@ -45,17 +45,18 @@ export default defineConfig({
       name: 'Sistran',
       use: {
         browserName: 'chromium',
-        channel: 'chrome', // Usa o Chrome instalado no sistema
+        // Remove channel e executablePath para usar o Chromium do Playwright
         launchOptions: {
-          executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', // Caminho padrão do Chrome
           args: [
-            '--no-sandbox', // Desativa o sandbox (se necessário)
+            '--no-sandbox', // Desativa o sandbox (necessário para Jenkins)
             '--disable-setuid-sandbox', // Desativa setuid sandbox
             '--disable-dev-shm-usage', // Desativa /dev/shm usage
             '--disable-accelerated-2d-canvas', // Desativa aceleração 2D canvas
             '--no-first-run', // Pula a primeira execução
             '--no-zygote', // Desativa zygote
             '--disable-gpu', // Desativa GPU
+            '--disable-web-security', // Desativa segurança web (se necessário)
+            '--disable-features=VizDisplayCompositor', // Desativa compositor
           ],
         },
       },
